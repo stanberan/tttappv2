9ba7b5e4334cf51886a8656f26484522f2e00b47
swordeath <s.beran@abdn.ac.uk>
2014-10-13 00:47:51 +0100

    folder change

--- t3v2/src/uk/ac/abdn/t3/t3v2app/JSONResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.abdn.t3.t3v2app;
-
-import org.json.JSONObject;
-
-public interface JSONResponseHandler {
-
-	
-	public void handleResponse(JSONObject o);
-}
--- t3v2/src/uk/ac/abdn/t3/t3v2app/holders/Capability.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.abdn.t3.t3v2app.holders;
-
-public class Capability {
-
-}
